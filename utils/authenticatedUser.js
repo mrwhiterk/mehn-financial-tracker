a74e93620b55a607989dc66f3a090215492cbe4c
Ryan White <mrwhiterk@gmail.com>
2019-02-26 07:10:33 -0500

    Added authenticated user and user.js

---
+++ utils/authenticatedUser.js
@@ -0,0 +1,7 @@
+module.exports = function authenticatedUser(req, res, next) {
+  if (req.isAuthenticated()) return next()
+
+  req.flash("info", "You must be signed in to view that page")
+
+  res.redirect("/login")
+}
