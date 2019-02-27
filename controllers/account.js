ef3985df336f3295bd76a059abbc4193e48e294d
Ryan White <mrwhiterk@gmail.com>
2019-02-27 10:29:06 -0500

    got account edit action working

--- controllers/account.js
+++ controllers/account.js
@@ -37,6 +37,21 @@ module.exports = {
   },
   new: (req, res) => {
     res.render('account/new')
+  },
+  update: (req, res) => {
+    Account.findOne({
+      _id: req.params.id
+    }).then(account => {
+      account.name = req.body.account.name
+      account.type = req.body.account.type
+      account.lastFour = req.body.account.lastFour
+      account.balance = req.body.account.balance
+
+      account.save(err => {
+        if (err) return res.status(500).send(err)
+        res.redirect(`/account/${account._id}`)
+      })
+    })
   }
 
 }
--- routes/account.js
+++ routes/account.js
@@ -5,7 +5,7 @@ const accountController = require("../controllers/account")
 router.post("/", accountController.create)
 router.get("/new", accountController.new)
 router.get("/:id", accountController.show)
-// router.put("/:id", accountController.update)
+router.put("/:id", accountController.update)
 // router.delete("/:id", accountController.delete)
 
 module.exports = router
